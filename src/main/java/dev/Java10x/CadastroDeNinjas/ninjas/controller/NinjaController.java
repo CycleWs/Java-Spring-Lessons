63ba63376902f927f596533bed278d8e66bd6a13
CycleWs <joaohlspj@gmail.com>
2025-08-18 18:47:42 -0300

    feat: Altera a rota do controller para listar os ninjas e injetar dependencia do service e do repository

--- src/main/java/dev/Java10x/CadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -1,16 +1,59 @@
 package dev.Java10x.CadastroDeNinjas.ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.Java10x.CadastroDeNinjas.ninjas.models.NinjaModel;
+import dev.Java10x.CadastroDeNinjas.ninjas.services.NinjaServices;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //todo CONTROLLER SÃO AS CLASSES ONDE VOCê IRÁ CONFIGURAR AS ROTAS
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaServices ninjaServices;
+
+    public NinjaController(NinjaServices ninjaServices) {
+        this.ninjaServices = ninjaServices;
+    }
+
+    //todo ENDPOINTS
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota!";
     }
+
+    //todo adcionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+
+        return "Ninja Criado";
+    }
+
+    //todo procurar ninja por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjasPorId(){
+        return "Ninjas por ID";
+    }
+
+    //todo Mostrar todos os ninjas (READ)
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas(){
+        return ninjaServices.listarNinjas();
+    }
+
+    //todo Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Ninja alterado por ID ";
+    }
+
+    //todo Deletar ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Ninja Deletado por Id";
+    }
+
+    //todo  > CRUD <
 }
---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/ninjas/services/NinjaServices.java
@@ -0,0 +1,29 @@
+package dev.Java10x.CadastroDeNinjas.ninjas.services;
+
+import dev.Java10x.CadastroDeNinjas.ninjas.models.NinjaModel;
+import dev.Java10x.CadastroDeNinjas.ninjas.repository.NinjaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NinjaServices {
+
+    //@Autowired //todo Cria um construtor automaticamente para injeção de dependencia
+    private NinjaRepository ninjaRepository;
+
+    public NinjaServices(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //todo Lógica para listar todos os ninjas
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    public Optional<NinjaModel> listarNinjaById(Long id){
+        return ninjaRepository.findById(id);
+    }
+
+}
